1d6b3b22a7db36692efea1c3265f4af7cab8fed9
Filip Stachura <filip@appsilondatascience.com>
2020-02-18 16:09:16 +0100

    Basic chart for Europe / animals.

--- src/plot_animals.R
+++ src/plot_animals.R
@@ -1,48 +1,49 @@
-library(plotly)
+library(sf)
+library(ggplot2)
+library(viridis)
+library(dplyr)
+source("./src/read_data.R")
 
-g <- list(
-  scope = 'europe',
-  showframe = F,
-  showland = T,
-  landcolor = toRGB("grey90")
-)
+world_geojson_loc <- file.path(getwd(), "geojson/world.geojson")
+gsf <- read_sf(paste0(readLines(world_geojson_loc), collapse = ""))
 
-g1 <- c(
-  g,
-  resolution = 50,
-  showcoastlines = T,
-  countrycolor = toRGB("white"),
-  coastlinecolor = toRGB("white"),
-  projection = list(type = 'Mercator'),
-  list(lonaxis = list(range = c(-15, -5))),
-  list(lataxis = list(range = c(0, 12))),
-  list(domain = list(x = c(0, 1), y = c(0, 1)))
-)
+empty_data <- function(df) {
+  all(c(df$"Number of animal species" == 0, df$"Number of animal breeds" == 0))
+}
 
-g2 <- c(
-  g,
-  showcountries = F,
-  bgcolor = toRGB("white", alpha = 0),
-  list(domain = list(x = c(0, .6), y = c(0, .6)))
-)
+missing <- animal_genetic_resources[!(animal_genetic_resources$Country_ISO %in% gsf$ISO_A3), ]
+if (!empty_data(missing)) {
+  warning("Geojson don't have shapefiles for non-empty countries!")
+  print(missing)
+}
 
-p <- df %>%
-  plot_geo(
-    locationmode = 'country names', sizes = c(1, 600), color = I("black")
-  ) %>%
-  add_markers(
-    y = ~Lat, x = ~Lon, locations = ~Country,
-    size = ~Value, color = ~abbrev, text = ~paste(Value, "cases")
-  ) %>%
-  add_text(
-    x = 21.0936, y = 7.1881, text = 'Africa', showlegend = F, geo = "geo2"
-  ) %>%
-  add_trace(
-    data = df9, z = ~Month, locations = ~Country,
-    showscale = F, geo = "geo2"
-  ) %>%
-  layout(
-    title = 'Ebola cases reported by month in West Africa 2014<br> Source: <a href="https://data.hdx.rwlabs.org/dataset/rowca-ebola-cases">HDX</a>'
+animal_genetic_clean <- filter(animal_genetic_resources, !(Country_ISO %in% missing$Country_ISO))
+gsf_europe <- gsf[gsf$ISO_A3 %in% animal_genetic_clean$Country_ISO, ]
+
+animal_short <- animal_genetic_clean %>%
+  mutate(number_species = `Number of animal species`) %>%
+  mutate(number_breeds = `Number of animal breeds`) %>%
+  select(Country_ISO, number_species, number_breeds)
+
+full_spdf <- merge(gsf_europe, animal_short, by.x = "ISO_A3", by.y = "Country_ISO") %>%
+  filter(number_species > 0)
+
+ggplot(data = full_spdf) +
+  geom_sf(aes(fill = number_species)) +
+  theme_void() +
+  scale_fill_viridis(breaks = c(1,5,10,20,50,100), name = "Number of animal species", guide = guide_legend(keyheight = unit(3, units = "mm"), keywidth = unit(12, units = "mm"), label.position = "bottom", title.position = 'top', nrow = 1)) +
+  labs(
+    title = "South of France Restaurant concentration",
+    subtitle = "Number of restaurant per city district"
+  ) +
+  theme(
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+
+    plot.title = element_text(size= 22, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
+    plot.subtitle = element_text(size= 17, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+
+    legend.position = c(1.7, 0.99)
   )
-
-p
